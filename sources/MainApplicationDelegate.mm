ca4d0fa4a8d274b33b83dca77ec5ac23503b37c4
kuyk15820-cpu <kuyk15820@gmail.com>
2026-07-02 23:33:08 +0700

    Update MainApplicationDelegate.mm

--- sources/MainApplicationDelegate.mm
+++ sources/MainApplicationDelegate.mm
@@ -8,10 +8,10 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor blackColor];
     
     if (@available(iOS 13.0, *)) {
-        self.window.overrideUserInterfaceStyle = UIUserInterfaceStyleLight;
+        self.window.overrideUserInterfaceStyle = UIUserInterfaceStyleDark;
     }
     
     _rootViewController = [[RootViewController alloc] init];
